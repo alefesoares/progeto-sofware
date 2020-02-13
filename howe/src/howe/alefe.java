1db55760d531f137b6afe0036975d2808d8e11c5
alefe.guimaraes <alefe.guimaraes@LAB2-DCX-14.labs.dcx>
2020-02-13 11:02:22 -0300

    commite04

--- howe/src/howe/alefe.java
+++ howe/src/howe/alefe.java
@@ -5,10 +5,12 @@ public class alefes{
 int alefe = 0;
 int halef = 0;
 int allef = 32;
+int aleffe = 0;
 }
 public  void somaAlefe(int alefe, int halef) {
  int vai = alefe+halef;
 System.out.println(vai);
+
 }
 
 }
